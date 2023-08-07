a6e186369775ac909e377529a5487f17fac96e5a
Marcelo <marcheloomeneses1@gmail.com>
2023-08-07 06:37:45 -0300

    change server api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
-export const API_BASE_URL = "https://orgabank.onrender.com/";
+export const API_BASE_URL =
+  /* "https://orgabank.onrender.com/" */ "https://api-orgabank-ioan-dev.fl0.io/";
 
 export const API_DEV_URL = `http://localhost:3001/`;
