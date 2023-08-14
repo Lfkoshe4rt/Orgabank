72d94dbed50d40f8a6d57edeb883575de9061db5
Marcelo <marcheloomeneses1@gmail.com>
2023-08-14 05:38:52 -0300

    add icons manifest update file vite.config.js and index.html

--- vite.config.js
+++ vite.config.js
@@ -16,22 +16,59 @@ export default defineConfig({
       },
       manifest: {
         name: "Orga Bank",
-        short_name: "ORG BNK",
+        short_name: "ORG BK",
+        theme_color: "#fff",
+        background_color: "#fff",
+        display: "standalone",
+        orientation: "portrait",
+        scope: "/",
+        start_url: "/",
         description: "Orga Bank",
-        theme_color: "#ffffff",
+
         icons: [
           {
-            src: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+            src: "/src/assets/images/icons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "/src/assets/images/icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "/src/assets/images/icons/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+          {
+            src: "/src/assets/images/icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "/src/assets/images/icons/icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+          },
+          {
+            src: "/src/assets/images/icons/icon-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+            src: "/src/assets/images/icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/src/assets/images/icons/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
         ],
       },
+
       devOptions: {
         enabled: true,
       },
