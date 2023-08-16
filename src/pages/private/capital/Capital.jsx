d6535ccbf85778da644a027aedcaf937031f7d12
Marcelo <marcheloomeneses1@gmail.com>
2023-08-16 05:11:47 -0300

    add file style page capital

--- src/pages/private/capital/Capital.jsx
+++ src/pages/private/capital/Capital.jsx
@@ -1,69 +1,21 @@
-import styled from "styled-components";
+import { useState } from "react";
+import { ToastContainer } from "react-toastify";
 import { CardMetric } from "../../../components/cardMetric";
 import { MovementGraph } from "../../../components/graphics/MovementGraph";
+import { Modal } from "../../../components/modal";
+import { FormNewMovement } from "../../../components/resource-form/formNewMovement";
 import { ScrollToUp } from "../../../components/scrollToUp";
 import { MovementTable } from "../../../components/tables/MovementTable";
 import { useAppSelector } from "../../../hooks/store";
-import { Modal } from "../../../components/modal";
-import { useState } from "react";
-import { FormNewMovement } from "../../../components/resource-form/formNewMovement";
-import { ToastContainer } from "react-toastify";
-
-const MainContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-top: 20px;
-
-  @media (max-width: 731px) {
-    flex-direction: column;
-    gap: 20px;
-  }
-`;
-
-const CardContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  gap: 20px;
-
-  @media (max-width: 731px) {
-    margin: 20px 0;
-    gap: 20px;
-    padding: 0;
-  }
-`;
-
-const ButtonAddMovement = styled.button`
-  padding: 6px 40px;
-  border: 1px solid rgb(76, 155, 80);
-  border-radius: 5px;
-  background-color: rgb(76, 175, 80);
-  color: white;
-  font-size: 1rem;
-  font-weight: 600;
-  cursor: pointer;
-  transition: all 0.3s ease-in-out;
-
-  &:hover {
-    background-color: rgb(76, 155, 80);
-  }
-
-  @media (max-width: 320px) {
-    width: 100%;
-    padding: 3px;
-  }
-`;
-
-const ButtonAddContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  margin-bottom: 20px;
-  margin-top: 10px;
-`;
+import {
+  ButtonAddContainer,
+  ButtonAddMovement,
+  CardContainer,
+  MainContainer,
+} from "./styled";
 
 export default function Dashboard() {
   const [openModal, setOpenModal] = useState(false);
-  const { _id } = useAppSelector((state) => state.user);
   const { movements } = useAppSelector((state) => state.movement);
   const toggleModal = () => setOpenModal(!openModal);
 
---
+++ src/pages/private/capital/styled/styled.jsx
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+
+  @media (max-width: 731px) {
+    flex-direction: column;
+    gap: 20px;
+  }
+`;
+
+export const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 20px;
+
+  @media (max-width: 731px) {
+    margin: 20px 0;
+    gap: 20px;
+    padding: 0;
+  }
+`;
+
+export const ButtonAddMovement = styled.button`
+  padding: 6px 40px;
+  border: 1px solid rgb(76, 155, 80);
+  border-radius: 5px;
+  background-color: rgb(76, 175, 80);
+  color: white;
+  font-size: 1rem;
+  font-weight: 600;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    background-color: rgb(76, 155, 80);
+  }
+
+  @media (max-width: 320px) {
+    width: 100%;
+    padding: 3px;
+  }
+`;
+
+export const ButtonAddContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 20px;
+  margin-top: 10px;
+`;
