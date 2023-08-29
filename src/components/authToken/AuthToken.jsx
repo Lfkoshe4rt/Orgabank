36646384793ba65a3c257ec1a3804071f32e3760
Marcelo <marcheloomeneses1@gmail.com>
2023-08-29 01:52:27 -0300

    fix useState timer

--- src/components/authToken/AuthToken.jsx
+++ src/components/authToken/AuthToken.jsx
@@ -3,9 +3,10 @@ import { useEffect, useState } from "react";
 import { useAppSelector } from "../../hooks/store";
 import { useCajaActions } from "../../hooks/useCajaActions";
 import { useUserActions } from "../../hooks/useUserActions";
-let myTimer;
 
 const AuthToken = ({ children }) => {
+  const [timer, setTimer] = useState(null);
+
   const { token } = useAppSelector((state) => state.user);
   const { refreshUser } = useUserActions();
   const { refreshCaja } = useCajaActions();
@@ -13,16 +14,17 @@ const AuthToken = ({ children }) => {
   useEffect(() => {
     if (token) {
       const decodedToken = jwt_decode(token);
-      myTimer = setTimeout(() => {
+
+      const myTimer = setTimeout(() => {
         refreshUser();
         refreshCaja();
         alert("Sesión expirada, por favor vuelva a iniciar sesión");
       }, decodedToken.exp * 1000 - Date.now());
+
+      setTimer(myTimer);
     }
 
-    if (!token) {
-      clearTimeout(myTimer);
-    }
+    if (!token) clearTimeout(timer);
   }, [token]);
 
   return children;
