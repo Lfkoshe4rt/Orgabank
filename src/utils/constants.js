3b1d4ce628bc5273301fca7f8b0a4ca28ba02949
Marcelo Meneses <marcheloomeneses1@gmail.com>
2023-11-09 21:51:05 -0300

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
-  /* "https://orgabank.onrender.com/" */ "https://api-orgabank-ioan-dev.fl0.io/";
+  /* "https://orgabank.onrender.com/" */ "https://api-orgabank-dev-tjfj.1.us-1.fl0.io/";
 
 export const API_DEV_URL = `http://localhost:3001/`;
